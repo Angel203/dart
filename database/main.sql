4729391f76d502b7068d5b3d2e6bc37592952509
DESKTOP-8P7ISG9\Stefan <st.engelmayer@gmail.com>
2017-12-26 22:04:38 +0100

    database updated

--- database/main.sql
+++ database/main.sql
@@ -9,7 +9,7 @@ Target Server Type    : SQLite
 Target Server Version : 30706
 File Encoding         : 65001
 
-Date: 2017-12-22 13:18:34
+Date: 2017-12-26 22:02:55
 */
 
 PRAGMA foreign_keys = OFF;
@@ -20,25 +20,23 @@ PRAGMA foreign_keys = OFF;
 DROP TABLE "main"."games";
 CREATE TABLE "games" (
 "id"  INTEGER NOT NULL,
-"date"  TEXT(255) NOT NULL,
+"gamedate"  TEXT(255) NOT NULL,
 "p1_id"  INTEGER NOT NULL,
-"p2_id"  INTEGER NOT NULL,
-"p3_id"  INTEGER,
-"p4_id"  INTEGER,
-"P5_id"  INTEGER,
-"P6_id"  INTEGER,
+"p2_id"  INTEGER DEFAULT -1,
+"p3_id"  INTEGER DEFAULT -1,
+"p4_id"  INTEGER DEFAULT -1,
+"P5_id"  INTEGER DEFAULT -1,
+"P6_id"  INTEGER DEFAULT -1,
+"set_id"  INTEGER NOT NULL,
 PRIMARY KEY ("id" ASC),
 CONSTRAINT "Player_ID_1" FOREIGN KEY ("p1_id") REFERENCES "player" ("id"),
 CONSTRAINT "Player_ID_2" FOREIGN KEY ("p2_id") REFERENCES "player" ("id"),
 CONSTRAINT "Player_ID_3" FOREIGN KEY ("p3_id") REFERENCES "player" ("id"),
 CONSTRAINT "Player_ID_4" FOREIGN KEY ("p4_id") REFERENCES "player" ("id"),
 CONSTRAINT "Player_ID_5" FOREIGN KEY ("P5_id") REFERENCES "player" ("id"),
-CONSTRAINT "Player_ID_6" FOREIGN KEY ("P6_id") REFERENCES "player" ("id")
+CONSTRAINT "Player_ID_6" FOREIGN KEY ("P6_id") REFERENCES "player" ("id"),
+CONSTRAINT "Set" FOREIGN KEY ("set_id") REFERENCES "sets" ("id")
 );
-
--- ----------------------------
--- Records of games
--- ----------------------------
 
 -- ----------------------------
 -- Table structure for "main"."player"
@@ -48,12 +46,19 @@ CREATE TABLE "player" (
 "id"  INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
 "name"  TEXT(50) NOT NULL,
 "surname"  TEXT(50) NOT NULL,
-"nickname"  TEXT(50)
+"nickname"  TEXT(50) NOT NULL
 );
 
+
 -- ----------------------------
--- Records of player
+-- Table structure for "main"."sets"
 -- ----------------------------
+DROP TABLE "main"."sets";
+CREATE TABLE "sets" (
+"id"  INTEGER NOT NULL,
+"wins_needed"  INTEGER NOT NULL,
+PRIMARY KEY ("id" ASC)
+);
 
 -- ----------------------------
 -- Table structure for "main"."sqlite_sequence"
@@ -61,9 +66,6 @@ CREATE TABLE "player" (
 DROP TABLE "main"."sqlite_sequence";
 CREATE TABLE sqlite_sequence(name,seq);
 
--- ----------------------------
--- Records of sqlite_sequence
--- ----------------------------
 
 -- ----------------------------
 -- Table structure for "main"."throws"
@@ -78,7 +80,3 @@ PRIMARY KEY ("id" ASC),
 CONSTRAINT "Game_ID" FOREIGN KEY ("game_id") REFERENCES "games" ("id"),
 CONSTRAINT "Player_ID" FOREIGN KEY ("player_id") REFERENCES "player" ("id")
 );
-
--- ----------------------------
--- Records of throws
--- ----------------------------
